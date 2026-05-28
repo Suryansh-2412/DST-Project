a5c533ab97ce9d5fed196f75fd9fe9070bdf2e55
CR4ZYM4D <madhavdixit482@gmail.com>
2026-05-28 16:16:18 +0530

    getPatient function

--- backend/src/routes/dashboardRoute.ts
+++ backend/src/routes/dashboardRoute.ts
@@ -4,6 +4,8 @@ import patientDashboard from "../controllers/patientDashboard.js"
 import checkInFunction from "../controllers/pateintCheckin.js"
 
 import doctorDashboard from "../controllers/doctorDashboard.js"
+import getPatients from "../controllers/doctorPatients.js"
+
 import { authMiddleware } from "../middleware/auth.js"
 
 const router = express.Router()
@@ -12,19 +14,19 @@ const router = express.Router()
 router.get("/patient/",patientDashboard) 
 router.get("/patient/records", authMiddleware,fetchRecords)
 router.post("/patient/checkin", authMiddleware, checkInFunction)
-router.post("/patient/settings", updatePatientProfile)
-router.post("/patient/chat", chatWithBot)
+router.post("/patient/settings", authMiddleware, updatePatientProfile)
+router.post("/patient/chat", authMiddleware, chatWithBot)
 
 // doctor
 router.get("/doctor/", doctorDashboard)
-router.get("/doctor/patients", getPatients)
-router.get("/doctor/monitoring", monitorDetails)
-router.post("/doctor/settings", updateProfile)
-router.post("/doctor/addPatient", addPatient)
+router.get("/doctor/patients", authMiddleware,getPatients)
+router.get("/doctor/monitoring", authMiddleware, monitorDetails)
+router.post("/doctor/settings", authMiddleware, updateProfile)
+router.post("/doctor/addPatient", authMiddleware, addPatient)
 
 // admin
-router.get("/admin/", adminDashboard)
-router.post("/admin/verify", verifyDoctor)
-router.get("/admin/models", getModelDetails)
+router.get("/admin/", authMiddleware, adminDashboard)
+router.get("/admin/models", authMiddleware, getModelDetails)
+router.post("/admin/verify", authMiddleware, verifyDoctor)
 
 export default router
